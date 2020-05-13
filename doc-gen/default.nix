1acf90d41a571ded9ad3bfaa083b2ef6e3103b2f
Owen McGrath <7798336+owm111@users.noreply.github.com>
2020-05-13 16:53:54 -0500

    Update doc generator

--- doc-gen/default.nix
+++ doc-gen/default.nix
@@ -1,3 +1,3 @@
 {
-  docsShell = (import (builtins.fetchTarball https://github.com/owm111/vim-doc-gen/archive/6d3aa3be89b5e58169686211b4b0f19d07632846.tar.gz)).shell;
+  docsShell = (import (builtins.fetchTarball https://github.com/owm111/vim-doc-gen/archive/55519d82b9210209ac439a5316f36e88e6321eaf.tar.gz)).shell;
 }
